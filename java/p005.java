59cc67121a6b4e3d3a94909f3cc704552fae65a5
Craig Ferguson <mediocrecraig@gmail.com>
2017-06-27 21:31:56 +0100

    Additional code documentation

--- java/p005.java
+++ java/p005.java
@@ -1,5 +1,6 @@
 /**
- * 
+ * What is the smallest positive number that is evenly divisible by all of the
+ * numbers from 1 to 20?
  * https://projecteuler.net/problem=5
  * 
  * @author cf443
@@ -7,6 +8,12 @@
 
 public final class p005 implements Solution {
 	
+	/*
+	 * Iterate through integers in the range [2,20], taking an accumulating
+	 * argument and in each case replacing it with the lowest common multiple
+	 * of the integer and the accumulator. The final number will be the smallest
+	 * integer which is divisible by all of the numbers from 1 to 20.
+	 */
 	@Override
 	public String run() {
 		int n = 1;
