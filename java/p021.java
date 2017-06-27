0e9608a019a4c46b85ff3a70f273ad89be5a3626
Craig Ferguson <mediocrecraig@gmail.com>
2017-06-27 21:52:30 +0100

    Additional code documentation

--- java/p021.java
+++ java/p021.java
@@ -1,24 +1,40 @@
-//https://projecteuler.net/problem=19
-class problem21 {
-	public static void main(String... ignored) {
-		long startTime = System.nanoTime();
+/**
+ * Let d(n) be defined as the sum of proper divisors of n (numbers less than n
+ * which divide evenly into n). If d(a) = b and d(b) = a, where a != b, then a 
+ * and b are an amicable pair and each of a and b are called amicable numbers.
+ * 
+ * Evaluate the sum of all the amicable numbers under 10000.
+ * 
+ * @author cf443
+ */
+
+public final class p021 implements Solution {
+	
+	/*
+	 * Iterate through all of the integers less than 1e4 and test whether
+	 * each once is amicable using the definition by brute force, keeping
+	 * an accumulator.
+	 */
+	public String run() {
+
+		long sum = 0, //Accumulator 
+			 a = 0, 
+			 b = 0;
 		
-		long sum = 0, a = 0, b = 0;
-		
+		//Iterate through all of the integers less than 1e4
 		for (int i = 1; i < 10000; i++) {
-			a = d(i);
-			b = d(a);
-			if (i==b && a!=b) {
-				System.out.println("Found pair: "+a+" : "+b);
-				sum+=a+b;
-			}
+			a = divisors(i);
+			b = divisors(a);
+			
+			if (i==b && a!=b) sum+=a+b; //Add both numbers to the accumulator
 		}
-		System.out.println(sum/2);
-		System.out.println("Process completed in "+(System.nanoTime()-startTime)/1000/1000.00+"ms.");
+		
+		return Long.toString(sum/2); //Halve to account for double-counting
+		
 	}
 	
 	//The sum of proper divisors of long n
-	public static long d(long n) {
+	private static long divisors(long n) {
 		
 		int sum = 0;
 		for (double i = 1; i <= Math.sqrt(n); i++) {
