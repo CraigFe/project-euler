ddf18eb46a0e7819e74ad804e10f61e5bd921b3b
Craig Ferguson <mediocrecraig@gmail.com>
2017-07-06 02:05:59 +0100

    P029: Improved implementation by using Set

--- java/p029.java
+++ java/p029.java
@@ -1,25 +1,30 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
-class problem29 {
+/**
+ * How many distinct terms are in the sequence given by a^b for 2 <= a <= 100 and 2 <= b <= 100?
+ * https://projecteuler.net/problem=29
+ * 
+ * @author cf443
+ */
+public final class p029 implements Solution {
 	
-	public static void main(String... ignored) {
-		long startTime = System.nanoTime();
-
-		List<Double> results = new ArrayList<Double>();
-		for (int a=2; a<=100; a++) {
-			for (int b=2; b<=100; b++) {
-				
-				
-				double result = Math.pow(a, b);
-				if (!results.contains(result)) results.add(result);
-				
+	/*
+	 * We simply generate all possible values of a^b for a,b in the range [2,100] and add these
+	 * values to a set. Then return the size of the set once all values have been calculated. 
+	 * 
+	 * The use of a set handles duplicate values automatically.
+	 */
+	@Override
+	public String run() {
+		Set<Double> results = new HashSet<>();
+		
+		for (int a = 2; a <= 100; a++) {
+			for (int b = 2; b <= 100; b++) {
+				results.add(Math.pow(a, b));
 			}
 		}
 		
-		System.out.println(results.size());
-		System.out.println("Process completed in "+(System.nanoTime()-startTime)/1000/1000.00+"ms.");
+		return Integer.toString(results.size());
 	}
-	
-
 }
