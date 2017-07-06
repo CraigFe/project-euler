b7c4d2f1ce67648395c81c4b05e03fe80151d124
Craig Ferguson <mediocrecraig@gmail.com>
2017-07-06 01:23:50 +0100

    Improved nCr definition and documentation

--- java/p015.java
+++ java/p015.java
@@ -1,38 +1,39 @@
+/**
+ * Starting in the top left corner of a 20x20 grid, and only being able to move to the right and
+ * down, how many routes are there?
+ * 
+ * @author cf443
+ */
+
 import java.math.BigInteger;
 
-//https://projecteuler.net/problem=15
-class problem15 {
-	
-	/* Recursive definition of nCr - WORKING - 1000C500 in 5.158ms*/
-	public static void main(String... ignored) {
-		long start = System.nanoTime();
-		System.out.println(nCr(1000,500));
-		System.out.println("Finished in " + (System.nanoTime()-start)/1000/1000.00 + "ms.");
-	}
-	
-	//nC(r+1) = nCr * (n-r)/(r+1)
-	//nCr = nC(r-1) * (n+1-r)/r
-	public static BigInteger nCr(int n, int r) {
-		if (r == 0) return BigInteger.ONE;
-		return nCr(n,r-1).multiply(BigInteger.valueOf(n+1-r)).divide(BigInteger.valueOf(r));
-		//N.B. Calculate numerator first to avoid fractions
-	}
-	
-	// Recursive factorial - WORKING - 1000C500 in 9.721ms
-	public static void main2(String... ignored) {
+public final class p015 implements Solution {
 
-		long start = System.nanoTime();
-		int gridSize = 500;
-		System.out.println(nCr(2*gridSize,gridSize));
-		System.out.println("Finished in " + (System.nanoTime()-start)/1000/1000.00 + "ms.");
+	/*
+	 * This problem is easily solved by combinatorics. Travelling from the top-left corner to the
+	 * bottom-right corner of an N*N grid can be interpreted as a series of 2N choices of whether
+	 * to go downwards or rightwards. In total there must be 20 'right' choices, and 20 'down'
+	 * choices, and it does not matter in which order they appear. The number of ways of selecting
+	 * a combination of 20 elements from a set of 40 is given by choose(20,40), which is computed
+	 * here using a multiplicative formula.
+	 */
+	public String run() {
+		return nCr(40,20).toString();
 	}
 	
-	public static BigInteger factorial(long n) {
-		if (n==1) return BigInteger.ONE;
-		return factorial(n-1).multiply(BigInteger.valueOf(n));
-	}
+	/*
+	 * Multiplicative formula for calculating nCr:
+	 * 	nCr(n, r) = product from i=1 to r of (n+1-i)/i
+	 */
+	public static BigInteger nCr(int n, int r) {
+		BigInteger prod = BigInteger.ONE;
+		
+		//Numerator
+		for (int i = 0; i < r; i++) prod = prod.multiply(BigInteger.valueOf(n - i));
 	
-	public static BigInteger nCr(long n, long r) {
-		return factorial(n).divide(factorial(r).multiply(factorial(n-r)));
+		//Denominator
+		for (int i = 2; i <= r; i++) prod = prod.divide(BigInteger.valueOf(i));
+		
+		return prod;
 	}
 }
