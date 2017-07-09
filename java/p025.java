e3bac52a64f2ccf8610665004100866a8c9372d0
Craig Ferguson <mediocrecraig@gmail.com>
2017-07-09 23:57:02 +0100

    P025: Modified to use fast Fibonacci doubling

--- java/p025.java
+++ java/p025.java
@@ -12,7 +12,7 @@ public final class p025 implements Solution {
 	 * at around 4780. We could simply compute the Fibonacci numbers in the standard way and output
 	 * the first number which has 1000 digits, but it is cleverer to trust in the accuracy of the
 	 * exponential growth of fib(n) for large n and begin scanning integers in the vicinity of 4780
-	 * for large n.
+	 * for large n. Here I use a fast algorithm for calculating Fibonacci numbers for large n.
 	 */
 	@Override
 	public String run() {
@@ -20,12 +20,30 @@ public final class p025 implements Solution {
 		BigInteger[] fibs = {BigInteger.ONE, BigInteger.ONE, BigInteger.ONE};
 		BigInteger bound = BigInteger.TEN.pow(1000); //The smallest number with 1000 digits
 		
-		int n;
-		for (n = 3; bound.compareTo(fibs[2]) > 0; n++) {
-			fibs[0] = fibs[1];
-			fibs[1] = fibs[2];
-			fibs[2] = fibs[1].add(fibs[0]);
-		}
+		//Take a conservative lower bound of the estimate - 10
+		int n = 4770;
+		while (bound.compareTo(fib(n)) > 0) n++;
 		return Integer.toString(n);
+	}
+	
+	/*
+	 * Fast Fibonacci doubling, see https://github.com/cf443/coursework
+	 * 
+	 * Given F(0) = 0, F(1) = 1
+	 * 
+	 *	F(2n)   = F(n) * (2*F(n+1) - F(n))
+	 *  F(2n_1) = F(n+1)^2 + F(n)^2
+	 */
+	private static BigInteger fib(int n) {
+		if (n < 2) return BigInteger.valueOf(n);
+		
+		if (n % 2 == 0) {
+			return fib(n / 2)
+					.multiply( (fib(n / 2 + 1).multiply(BigInteger.valueOf(2))
+											  .subtract(fib(n))));
+		} else {
+			return fib(n / 2 + 1).pow(2)
+					.add((fib(n / 2).pow(2)));
+		}
 	}
 }
