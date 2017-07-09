c4ce97d88f3db3846b81aa24b3c8277dc159c23c
Craig Ferguson <mediocrecraig@gmail.com>
2017-07-09 22:46:05 +0100

    P040: Changed implementation to use StringBuilder

--- java/p040.java
+++ java/p040.java
@@ -1,22 +1,30 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.lang.Math;
 
-class problem40 {
+/** Champernowne's constant is an irrational decimal fraction obtained by concatenating the
+ * positive integers: 
+ *	0.12345678910111213...
+ *
+ * Find the product of the nth digits where n = 1e0, 1e1 ..., 1e6
+ * https://projecteuler.net/problem=40
+ * 
+ * @author cf443
+ */
+public final class p040 implements Solution {
 	
-	//Lazy brute force
-	public static void main(String... ignored) {
-		long startTime = System.nanoTime();
-		
-		String fraction = "";
-		for(int i = 1; fraction.length() < 1000000; i++) fraction+=String.valueOf(i);
+	/*
+	 * A simple brute-force solution which generates 1e6 digits of Champernowne's constant and then
+	 * calculates the product of the appropriate digits. Some small amout of efficiency is achieved
+	 * by using the StringBuilder class instead of repeatedly using the concatenation operator (+),
+	 * which creates a new String object in every statement it is used and is therefore costly.
+	 */
+	@Override
+	public String run() {
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; sb.length() < 1000000; i++) sb.append(i);
 		
 		int product = 1;
-		for(int k=0;k<=6;k++) {
-			product *= Integer.valueOf(fraction.substring((int) Math.pow(10, k)-1,(int) Math.pow(10, k)));
-		}
+		for(int k = 0; k <= 6; k++) product *= sb.charAt((int) (Math.pow(10, k) - 1)) - '0';
 
-		System.out.println(product);
-		System.out.println("Process completed in "+(System.nanoTime()-startTime)/1000/1000.00+"ms.");
+		return Integer.toString(product);
 	}
 }
