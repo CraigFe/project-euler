ffcdb1af32b0dae6d755b72aec59555e6616af84
Craig Ferguson <mediocrecraig@gmail.com>
2017-07-09 23:42:31 +0100

    P025: Improved documentation and formatting

--- java/p025.java
+++ java/p025.java
@@ -1,22 +1,31 @@
 import java.math.BigInteger;
 
-class problem25 {
-	public static void main(String... ignored) {
-		long startTime = System.nanoTime();
+/**
+ * Given fib(1) = 1 and fib(2) = 1, what is the smallest value of n such that fib(n) >= 10^1000?
+ * 
+ * @author cf443
+ */
+public final class p025 implements Solution {
+	
+	/* The Fibonacci numbers fgrow exponentially with a base of ~1.618. Thus, the value of fib(n)
+	 * increases by one digit every ~4.78 (log_10(1.618)) on average. The correct answer is thus
+	 * at around 4780. We could simply compute the Fibonacci numbers in the standard way and output
+	 * the first number which has 1000 digits, but it is cleverer to trust in the accuracy of the
+	 * exponential growth of fib(n) for large n and begin scanning integers in the vicinity of 4780
+	 * for large n.
+	 */
+	@Override
+	public String run() {
 		
-		BigInteger[] fibs = new BigInteger[3];
-		fibs[1] = BigInteger.ONE;
-		fibs[2] = BigInteger.ONE;
+		BigInteger[] fibs = {BigInteger.ONE, BigInteger.ONE, BigInteger.ONE};
+		BigInteger bound = BigInteger.TEN.pow(1000); //The smallest number with 1000 digits
 		
-		int digits = 0, nth = 1000, n = 3; //Initial values
-		for (; digits != nth; n++) {
+		int n;
+		for (n = 3; bound.compareTo(fibs[2]) > 0; n++) {
 			fibs[0] = fibs[1];
 			fibs[1] = fibs[2];
 			fibs[2] = fibs[1].add(fibs[0]);
-			digits = fibs[2].toString().length();
-			System.out.println("n: "+n+" - digits: "+digits+" - fib: "+fibs[2]);
 		}
-		System.out.println(n--);
-		System.out.println("Process completed in "+(System.nanoTime()-startTime)/1000/1000.00+"ms.");
+		return Integer.toString(n);
 	}
 }
